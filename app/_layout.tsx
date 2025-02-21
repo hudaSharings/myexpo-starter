f52640a5b917f1ee2d24855f279601b59c1f23d7
hudaSharings <hudapossible@gmail.com>
2025-02-22 02:25:39 +0530

    build issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,9 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { useStore } from "../store/useStore";
-
-SplashScreen.preventAutoHideAsync();
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-reanimated";
+//SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
@@ -23,8 +24,15 @@ export default function RootLayout() {
 */
 
   return (
+    <SafeAreaProvider>
     <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index"  />
+        <Stack.Screen name="(auth)/login" options={{ headerShown: true, title: "", headerBackButtonDisplayMode:"minimal" }} />
+        <Stack.Screen name="(auth)/register"options={{headerShown: true,title: "Register", headerBackButtonDisplayMode:"minimal" }} />
+        </Stack>
+      
     </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,41 @@
-import { View, Text, Button } from "react-native";
-import Swiper from "react-native-swiper";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 import { useStore } from "../store/useStore";
 import { useRouter } from "expo-router";
+import "react-native-reanimated";
+
 export default function Index() {
 
   const setHasSeenIntro = useStore((state:any) => state.setHasSeenIntro);
   const router = useRouter();
 
   return (
-    <Swiper loop={false} showsPagination={true}>
-      <View className="flex-1 justify-center items-center bg-blue-200">
-        <Text className="text-2xl">Welcome to the App!</Text>
-      </View>
-      <View className="flex-1 justify-center items-center bg-green-200">
-        <Button title="Get Started" onPress={() => {
+    
+      <View className="flex-1 justify-center items-center w-full bg-green-200 gap-5 p-4">
+        <TouchableOpacity  onPress={() => {
           setHasSeenIntro();
-          router.replace("/(auth)/login");
-        }} />
+          router.push("/(auth)/login");
+        }} >
+          <Text className="text-xl">Login</Text>
+        </TouchableOpacity>
+
+      <TouchableOpacity   onPress={() => {
+          setHasSeenIntro();
+          router.push("/(auth)/register");
+        }}>
+          <Text className="text-xl ">Register</Text>
+      </TouchableOpacity>
       </View>
-    </Swiper>
   );
  // return <Redirect href="/(tabs)/home" />
 }
+
+const styles = StyleSheet.create({
+  pagerView: {
+    flex: 1,
+  },
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
